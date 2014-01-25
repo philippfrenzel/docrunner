c10afec6b0cfa459a248665710983696ad747120
Philipp Frenzel <philipp@frenzel.net>
2014-01-25 17:07:35 +0100

    add phpthumblibrary

--- modules/dms/views/dmpaper/iviews/_item.php
+++ modules/dms/views/dmpaper/iviews/_item.php
@@ -6,6 +6,8 @@ use yii\helpers\HtmlPurifier;
 use yii\web\JsExpression;
 use kartik\icons\Icon;
 
+use \phpthumb;
+
 ?>
 
 <div class="dmpaper">
@@ -26,6 +28,28 @@ use kartik\icons\Icon;
   <div class="widget-body">
     <div class="row">
       <div class="col-md-6">
+        <div class="pull-left">
+          <?php
+            //testing the pdf thumb generator
+            $filename = "http://www.education.gov.yk.ca/pdf/pdf-test.pdf";
+            
+            $thumbler = new phpthumb();
+            $thumbler->setSourceData(file_get_contents($filename));
+            $thumbler->setParameter('w', '100');
+            
+            $output_filename = \Yii::$app->basePath."/attachements/thumb_small.jpg";
+            if ($thumbler->GenerateThumbnail()) { // this line is VERY important, do not remove it!
+                if ($thumbler->RenderToFile($output_filename)) {
+                        // do something on success
+                        echo 'Successfully rendered to "'.$output_filename.'"';
+                } else {
+                        // do something with debug/error messages
+                        echo 'Failed:<pre>'.implode("\n\n", $thumbler->debugmessages).'</pre>';
+                }
+                $thumbler->purgeTempFiles();
+            }
+          ?>
+        </div>
         <blockquote>
           <?= $model['description']; ?>
         </blockquote>
